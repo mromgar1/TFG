8a0c4293a9c9ebdb675d3b425b75137f8220843d
mromgar1 <mromgar1@upv.edu.es>
2026-06-04 16:47:43 +0200

    -

--- src/upvfab/sin300/cband/cells/wvl_tracker.py
+++ src/upvfab/sin300/cband/cells/wvl_tracker.py
@@ -322,18 +322,18 @@ from upvfab.sin300.cband.tech import LAYER, TECH
 #         width=width,
 #     )
 
-@gf.cell
-def die(dieL = 5000, dieW = 5000, border = 250, layer_box = "FLOORPLAN"):
-    # Die specifications (Chip)
-    box = gf.Component()
-    obox = box.add_ref(gf.components.rectangle(size=(dieW,dieL),layer=layer_box))
-    ibox = box.add_ref(gf.components.rectangle(size=(dieW-border*2,dieL-border*2),layer=layer_box)).dmovex(border).dmovey(border)
-    box = gf.boolean(A=obox, B=ibox, operation="A-B", layer=layer_box)
-    # Adding ports to a component 
-    box.add_port(name="block@org", center=[border,border], width=1, orientation=0, layer=layer_box)
-    box.add_port(name = "base", center = [dieW/2, border], width=1, orientation=0, layer=layer_box )
-    box.draw_ports()
-    return box
+# @gf.cell
+# def die(dieL = 5000, dieW = 5000, border = 250, layer_box = "FLOORPLAN"):
+#     # Die specifications (Chip)
+#     box = gf.Component()
+#     obox = box.add_ref(gf.components.rectangle(size=(dieW,dieL),layer=layer_box))
+#     ibox = box.add_ref(gf.components.rectangle(size=(dieW-border*2,dieL-border*2),layer=layer_box)).dmovex(border).dmovey(border)
+#     box = gf.boolean(A=obox, B=ibox, operation="A-B", layer=layer_box)
+#     # Adding ports to a component 
+#     box.add_port(name="block@org", center=[border,border], width=1, orientation=0, layer=layer_box)
+#     box.add_port(name = "base", center = [dieW/2, border], width=1, orientation=0, layer=layer_box )
+#     box.draw_ports()
+#     return box
 
 
 #########################################################################################
@@ -421,35 +421,35 @@ def die(dieL = 5000, dieW = 5000, border = 250, layer_box = "FLOORPLAN"):
 #ESTRUCTURAS TEST
 #########################################################################################
 
-@gf.cell
-def mmi_2x2_test(length_mmi_2x2: float = 262.9723, taper_width_mmi_2x2: float = 2.8, gap_mmi_2x2: float = 0.5,  taper_length = 10, width: float = TECH.width):
-    c = gf.Component()
+# @gf.cell
+# def mmi_2x2_test(length_mmi_2x2: float = 262.9723, taper_width_mmi_2x2: float = 2.8, gap_mmi_2x2: float = 0.5,  taper_length = 10, width: float = TECH.width):
+#     c = gf.Component()
 
-    mmi_95 = c << mmi2x2(width, taper_width_mmi_2x2, taper_length, length_mmi_2x2, 10, gap_mmi_2x2)
-    b1 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
-    b2 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
-    b1.mirror_x()
-    mmi_95.dmovex(b1.ports["o2"].dx - length_mmi_2x2 - taper_length).dmovey(b1.ports["o2"].dy + taper_width_mmi_2x2/2  + gap_mmi_2x2/2) 
-    b2.dmovex(mmi_95.ports["o3"].dx).dmovey(mmi_95.ports["o3"].dy)
+#     mmi_95 = c << mmi2x2(width, taper_width_mmi_2x2, taper_length, length_mmi_2x2, 10, gap_mmi_2x2)
+#     b1 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
+#     b2 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
+#     b1.mirror_x()
+#     mmi_95.dmovex(b1.ports["o2"].dx - length_mmi_2x2 - taper_length).dmovey(b1.ports["o2"].dy + taper_width_mmi_2x2/2  + gap_mmi_2x2/2) 
+#     b2.dmovex(mmi_95.ports["o3"].dx).dmovey(mmi_95.ports["o3"].dy)
 
-    b3 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
-    b4 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
-    b3.mirror_x()
-    b3.mirror_y()
-    b3.dmovex(mmi_95.ports["o1"].dx).dmovey(mmi_95.ports["o1"].dy)
-    b4.mirror_x()
-    b4.dmovex(mmi_95.ports["o2"].dx).dmovey(mmi_95.ports["o2"].dy)
+#     b3 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
+#     b4 = c << bend_s(size = [10, 15], cross_section = "strip", width = width, allow_min_radius_violation = True)
+#     b3.mirror_x()
+#     b3.mirror_y()
+#     b3.dmovex(mmi_95.ports["o1"].dx).dmovey(mmi_95.ports["o1"].dy)
+#     b4.mirror_x()
+#     b4.dmovex(mmi_95.ports["o2"].dx).dmovey(mmi_95.ports["o2"].dy)
 
-    c.add_port(name = "o1", port = b4.ports["o2"], port_type = "optical")
-    c.add_port(name = "o2", port = b3.ports["o2"], port_type = "optical")
-    c.add_port(name = "o3", port = b2.ports["o2"], port_type = "optical")
-    c.add_port(name = "o4", port = b1.ports["o1"], port_type = "optical")
+#     c.add_port(name = "o1", port = b4.ports["o2"], port_type = "optical")
+#     c.add_port(name = "o2", port = b3.ports["o2"], port_type = "optical")
+#     c.add_port(name = "o3", port = b2.ports["o2"], port_type = "optical")
+#     c.add_port(name = "o4", port = b1.ports["o1"], port_type = "optical")
 
-    #normalizar posición para que quede en (0,0)
-    x0, y0 = c.ports["o1"].dcenter
-    c.dmove((-x0, -y0))
+#     #normalizar posición para que quede en (0,0)
+#     x0, y0 = c.ports["o1"].dcenter
+#     c.dmove((-x0, -y0))
 
-    return c
+#     return c
 
 # @gf.cell
 # def mmi_3x3_test(length_mmi_3x3: float =  242.4723, taper_width_mmi_3x3: float = 2.8, gap_mmi_3x3: float = 0.2, taper_length: float = 10, altura_bends: float = 13.649999999999999, width: float = TECH.width): #altura bends heredado de cto: h_bends_33
@@ -485,27 +485,27 @@ def mmi_2x2_test(length_mmi_2x2: float = 262.9723, taper_width_mmi_2x2: float = 
 
 #     return c
 
-@gf.cell
-def spiral_delays_test(length_spiral: float = 2152.431640625, width : float = TECH.width, radius: float = TECH.radius ):
-    c = gf.Component()
-    spiral = c << spiral_upv(radius = radius, N_spr = 10 , d_SPR =10 , dx_SPR= length_spiral, dy_SPR = 10, layer = "strip") # N must BE EVEN 
-    h = spiral.ports["o2"].dx - spiral.ports["o1"].dx
-    f_bend_euler180 = partial(bend_euler,angle=180)
-    delay_1 = c << gf.components.delay_snake(length=h/2, length0=0, length2=0, n=2, bend180=f_bend_euler180(), cross_section='strip', width = width)
-    delay_2 = c << gf.components.delay_snake(length=h/2, length0=0, length2=0, n=2, bend180=f_bend_euler180(), cross_section='strip', width = width)
-    delay_1.mirror_x().mirror_y()
-    delay_1.dmovex(spiral.ports["o1"].dx ).dmovey(spiral.ports["o1"].dy) 
-    delay_2.mirror_y()
-    delay_2.dmovex(spiral.ports["o2"].dx ).dmovey(spiral.ports["o2"].dy )
+# @gf.cell
+# def spiral_delays_test(length_spiral: float = 2152.431640625, width : float = TECH.width, radius: float = TECH.radius ):
+#     c = gf.Component()
+#     spiral = c << spiral_upv(radius = radius, N_spr = 10 , d_SPR =10 , dx_SPR= length_spiral, dy_SPR = 10, layer = "strip") # N must BE EVEN 
+#     h = spiral.ports["o2"].dx - spiral.ports["o1"].dx
+#     f_bend_euler180 = partial(bend_euler,angle=180)
+#     delay_1 = c << gf.components.delay_snake(length=h/2, length0=0, length2=0, n=2, bend180=f_bend_euler180(), cross_section='strip', width = width)
+#     delay_2 = c << gf.components.delay_snake(length=h/2, length0=0, length2=0, n=2, bend180=f_bend_euler180(), cross_section='strip', width = width)
+#     delay_1.mirror_x().mirror_y()
+#     delay_1.dmovex(spiral.ports["o1"].dx ).dmovey(spiral.ports["o1"].dy) 
+#     delay_2.mirror_y()
+#     delay_2.dmovex(spiral.ports["o2"].dx ).dmovey(spiral.ports["o2"].dy )
 
-    c.add_port(name = "o1", port = delay_1.ports["o2"], port_type = "optical")
-    c.add_port(name = "o2", port = delay_2.ports["o2"], port_type = "optical")
+#     c.add_port(name = "o1", port = delay_1.ports["o2"], port_type = "optical")
+#     c.add_port(name = "o2", port = delay_2.ports["o2"], port_type = "optical")
 
-    #normalizar posición para que quede en (0,0)
-    x0, y0 = c.ports["o1"].dcenter
-    c.dmove((-x0, -y0))
+#     #normalizar posición para que quede en (0,0)
+#     x0, y0 = c.ports["o1"].dcenter
+#     c.dmove((-x0, -y0))
 
-    return c
+#     return c
 
 # @gf.cell
 # def mmi_ts(
